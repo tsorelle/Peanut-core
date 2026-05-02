159ab0d7b8d889f1d7504aeac066494a2ad4e987
tsorelle <terry.sorelle@outlook.com>
2026-05-02 11:24:02 -0500

    Content fetch and save stubbed out.

--- web.root/tq-peanut/pnut/packages/peanut-content/vm/ContentManagerViewModel.ts
+++ web.root/tq-peanut/pnut/packages/peanut-content/vm/ContentManagerViewModel.ts
@@ -58,6 +58,7 @@ namespace PeanutContent {
                             successFunction()
                         }
                     }
+
                 }
             ).fail(function () {
                 me.services.getErrorInformation();
